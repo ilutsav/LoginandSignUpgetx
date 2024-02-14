afc71c3211b2d89c6b2c87a7e822348a770760c9
ilutsav <ilutsav@gmail.com>
2024-02-14 13:16:10 +0545

    get request successful

--- lib/model/login_auth.dart
+++ lib/model/login_auth.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:login_signup_getxwith_postapi/controller/shared_prefrences.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginAuth {
   final prefrencesController = Get.put(PrefrencesManager());
   String? token;
+  int? userId;
   Future<bool> loginAuth(String email, String password) async {
     final Map<String, dynamic> requestbody = {
       'email': email,
@@ -23,11 +24,13 @@ class LoginAuth {
       if (response.statusCode == 200) {
         final Map<String, dynamic> responseData = json.decode(response.body);
         token = json.decode(response.body)['token'];
+        userId = json.decode(response.body)['userId'];
         print('login sucessful');
+        print(response.body);
         prefrencesController.saveToken(token);
         print('token = $token');
 
-        //getUserDetails();
+        getUserDetails();
         return true;
       } else {
         print('Error: ${response.statusCode} - ${response.body}');
@@ -38,34 +41,32 @@ class LoginAuth {
       return false;
     }
   }
+
+  Future<Map<String, dynamic>?> getUserDetails() async {
+    if (userId == null) {
+      print('Error: No userId available');
+      return null;
+    }
+
+    try {
+      final response = await http.get(
+        Uri.parse(
+          'https://backend-gilt-two.vercel.app/api/v1/getMentees/$userId',
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> userData = json.decode(response.body);
+        print('User details fetched successfully');
+        print('user data $userData');
+        return userData;
+      } else {
+        print('Error: ${response.statusCode} - ${response.body}');
+        return null;
+      }
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
 }
-  // Future<Map<String, dynamic>?> getUserDetails() async {
-  //   if (token == null) {
-  //     print('Error: No authentication token');
-  //     return null;
-  //   }
-
-  //   try {
-  //     final response = await http.get(
-  //       Uri.parse('https://backend-gilt-two.vercel.app/api/v1/getMentees'),
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         'Authorization': 'Bearer $token', // Include the token in the headers
-  //       },
-  //     );
-
-  //     if (response.statusCode == 200) {
-  //       final Map<String, dynamic> userData = json.decode(response.body);
-  //       print('User details fetched successfully');
-  //       print('user data $userData');
-  //       return userData;
-  //     } else {
-  //       print('Error: ${response.statusCode} - ${response.body}');
-  //       return null;
-  //     }
-  //   } catch (e) {
-  //     print(e);
-  //     return null;
-  //   }
-  // }
-
